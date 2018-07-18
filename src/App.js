4a77ee0bd2bc98f0bcb0ee739dbb956f5ba26cda
rfpoulos <rfpoulos@outlook.com>
2018-07-17 22:19:45 -0400

    recompose mapPropsStream working rxjs5

--- src/App.js
+++ src/App.js
@@ -2,20 +2,55 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { 
           compose,
+          mapPropsStream,
           withState,
-          withHandlers, 
+          withHandlers,
+          setObservableConfig,
       } from 'recompose'; 
 import './App.css';
+import Rx from 'rxjs';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import rxjsconfig from 'recompose/rxjsObservableConfig'
+setObservableConfig(rxjsconfig)
 
-let AppDumb = ({ input, setInput }) =>
-      <input type='text' value={input} onChange={setInput}></input>
 
-let App = compose(
-  withState('input', 'updateInput', ''),
-  withHandlers({
-    setInput: ({ updateInput }) => event =>
-      updateInput(event.target.value),
-  })
-)(AppDumb)
+let searchGithub = (term) =>
+    fetch(`https://api.github.com/search/users?q=${term}`)
+    .then(data => data.json());
 
-export default App;
+let App = ({ onSearch, results, query }) =>
+  <div>
+    <input type="text" value={ query } onChange={ (event) => onSearch(event.target.value) } />
+    <ul>{
+      results.items.map((result, key) =>
+        <li key={ key }>{result.login}</li>)
+    }</ul>
+  </div>
+      
+
+let enhance = compose(
+  mapPropsStream(props$ => {
+    let search$ = new Subject();
+    let onSearch = v => search$.next(v);
+
+    let query$ =  search$
+      .startWith('');
+
+    let results$ = query$
+      .debounceTime(200)
+      .distinctUntilChanged()
+      .switchMap(query => query ? 
+          searchGithub(query) : Promise.resolve({items: []})
+        );
+
+
+    return props$.combineLatest(results$, query$,
+      (props, results, query) => ({
+        ...props, onSearch, results, query
+      })
+    )}
+  )
+);
+
+export default enhance(App);
